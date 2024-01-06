f291cb61dbec6de9383c18275fbd7f6f42d1359d
Hridayy <73392185+Hridayy@users.noreply.github.com>
2024-01-06 17:20:06 +0530

    Add files via upload

---
+++ SEA of IT.py
@@ -0,0 +1,10 @@
+
+
+# a program to check the year is leap year or not
+
+a=int(input("Enter a year: "))
+if a%4==0:
+    print("This year is a leap year")
+else:
+    print("This year is not a leap year")
+    
